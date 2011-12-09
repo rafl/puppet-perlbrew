cc372356f1539e55cf1350e953655462dc3ad316
Florian Ragwitz <rafl@debian.org>
2011-12-09 09:59:59 +0100

    Ensure everything needed to build perl is present

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,15 @@
 class perlbrew::install {
+  package {
+    "build-essential":
+      ensure => present,
+  }
+
   file {
     "/usr/local/bin/perlbrew":
-      owner  => root,
-      group  => root,
-      mode   => 0755,
-      source => "puppet:///modules/perlbrew/perlbrew"
+      owner   => root,
+      group   => root,
+      mode    => 0755,
+      source  => "puppet:///modules/perlbrew/perlbrew"
+      require => Package["build-essential"],
   }
 }
