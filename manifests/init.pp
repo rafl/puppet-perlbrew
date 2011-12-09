16ede23655bfc6d90a821465fdfe5f15e0b3858a
Florian Ragwitz <rafl@debian.org>
2011-12-09 12:16:36 +0100

    Implement building perls

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,15 @@ class perlbrew {
   include perlbrew::params
   include perlbrew::install
   include perlbrew::environment
+
+  define build ($version) {
+    exec {
+      "perlbrew_build_${name}":
+        command => "/bin/sh -c 'umask 022; /usr/bin/env PERLBREW_ROOT=${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install ${version} --as ${name}'",
+        user    => "perlbrew",
+        group   => "perlbrew",
+        creates => "${perlbrew::params::perlbrew_root}/perls/${name}",
+        require => Class["perlbrew::environment"],
+    }
+  }
 }
