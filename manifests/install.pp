4738d448c608f1a635fdffd6eab3dd2457e3a8e0
Florian Ragwitz <rafl@debian.org>
2011-12-09 13:30:03 +0100

    Depend on wget for perl downloads

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,12 +4,17 @@ class perlbrew::install {
       ensure => present,
   }
 
+  package {
+    "wget":
+      ensure => present,
+  }
+
   file {
     $perlbrew::params::perlbrew_bin:
       owner   => root,
       group   => root,
       mode    => 0755,
       source  => "puppet:///modules/perlbrew/perlbrew"
-      require => Package["build-essential"],
+      require => [ Package["build-essential"], Package["wget"] ],
   }
 }
