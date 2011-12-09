5d6895fb48854668780536d8a73d271466ba13b4
Florian Ragwitz <rafl@debian.org>
2011-12-09 12:29:05 +0100

    Increase the timeout for building perl

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,7 @@ class perlbrew {
         command => "/bin/sh -c 'umask 022; /usr/bin/env PERLBREW_ROOT=${perlbrew::params::perlbrew_root} ${perlbrew::params::perlbrew_bin} install ${version} --as ${name}'",
         user    => "perlbrew",
         group   => "perlbrew",
+        timeout => 3600,
         creates => "${perlbrew::params::perlbrew_root}/perls/${name}",
         require => Class["perlbrew::environment"],
     }
