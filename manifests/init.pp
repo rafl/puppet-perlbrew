324053a2888124ef4561386ce3dee71f2faa7d86
Florian Ragwitz <rafl@debian.org>
2011-12-09 13:30:16 +0100

    Implement installing cpanm for perlbrews

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,4 +14,17 @@ class perlbrew {
         require => Class["perlbrew::environment"],
     }
   }
+
+  define install_cpanm () {
+    exec {
+      "install_cpanm_${name}":
+        # puppet seems to change the current user weirdly when using the
+        # user/group options. That causes cpanm to use /root/.cpanm for it's
+        # temporary storage, which happens to not be writable for the perlbrew
+        # user. Use /bin/su to work this around.
+        command => "/bin/su - -c 'umask 022; wget -O- ${perlbrew::params::cpanm_url} | ${perlbrew::params::perlbrew_root}/perls/${name}/bin/perl - App::cpanminus' perlbrew",
+        creates => "${perlbrew::params::perlbrew_root}/perls/${name}/bin/cpanm",
+        require => Perlbrew::Build[$name],
+    }
+  }
 }
