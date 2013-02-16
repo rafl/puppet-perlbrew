76df94fd59a9c670dcf833a9342d3f9360cd6a81
Ulrich Habel <rhaen@cpan.org>
2013-02-16 13:37:29 +0000

    Added basic rspec infrastructure/travis ci

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,20 @@
+# Class: perlbrew::params
+#
+#   This class contains some sane defintions for variables. This class
+#   must not be used directly, it gets loaded by the other classes.
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
 class perlbrew::params {
-  $perlbrew_root = "/usr/local/perlbrew"
-  $perlbrew_bin = "/usr/local/bin/perlbrew"
-  $cpanm_url = "http://github.com/miyagawa/cpanminus/raw/master/cpanm"
+
+  $perlbrew_root = '/usr/local/perlbrew'
+  $perlbrew_bin  = '/usr/local/bin/perlbrew'
+  $cpanm_url     = 'http://github.com/miyagawa/cpanminus/raw/master/cpanm'
+
 }
