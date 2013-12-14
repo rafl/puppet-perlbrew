8ffb8212f6e0a15fbe1391932e6949824dcfe1fb
Rob Kinyon <rob.kinyon@gmail.com>
2013-12-14 00:03:55 -0500

    Added patch-perl installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,19 @@ class perlbrew {
   include perlbrew::install
   include perlbrew::environment
 
+  define install_patchperl () {
+    exec {
+        # puppet seems to change the current user weirdly when using the
+        # user/group options. That causes cpanm to use /root/.cpanm for it's
+        # temporary storage, which happens to not be writable for the perlbrew
+        # user. Use /bin/su to work this around.
+      "install_patchperl_${name}":
+        command => "/bin/su - -c 'umask 022; ${perlbrew::params::perlbrew_bin} install-patchperl'",
+        creates => "${perlbrew::params::perlbrew_root}/bin/patchperl",
+        require => Class['perlbrew::environment'],
+    }
+  }
+
   define build ($version) {
     exec {
       "perlbrew_build_${name}":
@@ -24,7 +37,10 @@ class perlbrew {
         group   => 'perlbrew',
         timeout => 3600,
         creates => "${perlbrew::params::perlbrew_root}/perls/${name}",
-        require => Class['perlbrew::environment'],
+        require => [
+          Class['perlbrew::environment'],
+          Perlbrew::Install_patchperl,
+        ],
     }
   }
 
